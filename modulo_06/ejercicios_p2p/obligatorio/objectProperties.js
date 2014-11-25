66f226579c76aff2464ae15d88f0eded44556737
adlpzrmn <adlpzrmn@yahoo.es>
2014-11-25 01:52:33 +0100

    [HTML5 MOOC] [Módulo 6] [Ejercicios P2P]
    Ejercicio P2P Obligatorio Módulo 6

--- modulo_06/ejercicios_p2p/obligatorio/objectProperties.js
+++ modulo_06/ejercicios_p2p/obligatorio/objectProperties.js
@@ -1,69 +1,34 @@
-
-function propertiesPrint (objectName){
-
-  if (! objectName typeof Object) {
-
-
-  }
-
-
-       var currentProperty;
-       var propertiesTable = document.getElementById("propertiesTable")
-
-
-
-
-for (currentProperty in objectName){   //cada iteración genera una fila de la tabla
-
-    if ( currentProperty typeof Object) {
-
-var div
-
-    } else {
-  propertiesTable.innerHTML+="<tr><td>"+i+"</td><td> = "+objectName[i]+"</td></tr>";
-}
+function onchangeObjectName() {
+    var userMessage = document.getElementById("userMessage");
+    userMessage.style.display = 'none';
+    var propertiesList = document.getElementById("propertiesList");
+    propertiesList.style.display = 'none';
 }
 
-
-}
-
-
-
-
-
-
-// PROBLEMA: prompt trae el valor como un string, por eso === (identidad extricta o fuerte) considera que el string "1" y el número 1 son distintos.
-// SOLUCION: USara == (Identidad débil) o convertir el string a número.
-// La conversión no se puede hacer directamente después del prompt, por si el usuario mete un valor que no parsee con un número.
-// Debo hacerlo por ejemplo en la comparación.
-// Por otro lado en este caso la identidad débil == funcionaría bastante bien, pero como está desaconsejada, no la voy a usar.
-var numeroAdivinado = prompt("Introduce un número del 1 al 3, ambos inclusives:\n\n(輸入一個數字從1至3個，包括兩端:)");
-var numeroAleatorio = num();
-var numerosPermitidos = [1, 2, 3];
-var numerosPermitidosAsString = numerosPermitidos.valueOf();
-var encontrado = false;
-var numeroAdivinadoAsNumber;
-if (isNaN(numeroAdivinado) || numeroAdivinado.trim() === "") {
-    var mensajeError = "<h3 class=error>Debe introducir un número.</h3> <br> <h3 class=error>Los únicos números permitidos son: " + numerosPermitidosAsString + ".</h3>";
-    document.write(mensajeError);
-} else {
-    numeroAdivinadoAsNumber = +numeroAdivinado;
-    for (var i = 0; i < numerosPermitidos.length && !encontrado; i++) {
-        // NOTA: Realizo la conversión del string a número
-        encontrado = numerosPermitidos[i] === numeroAdivinadoAsNumber;
-    };
-    if (!encontrado) {
-        var mensajeError = "<h3 class=error>Debe introducir un número permitido.</h3> <br> <h3 class=error>Los únicos números permitidos son: " + numerosPermitidosAsString + ".</h3>";
-        document.write(mensajeError);
-    } else if (numeroAdivinadoAsNumber === numeroAleatorio) {
-        var mensajeError = "<h3>Correcto.</h3>"
-        document.write(mensajeError);
-    } else {
-        var mensajeError = "<h3>Lo siento el número era " + numeroAleatorio + "</h3>";
-        document.write(mensajeError);
+function propertiesPrint(objectName) {
+        //alert('objectProperties.propertiesPrint() -> INICIO.');
+        var objeto = window[objectName];
+        if ('object' !== typeof objeto) {
+            var userMessageInline = document.getElementById("userMessageInline");
+            userMessageInline.innerHTML = "El elemento introducido no es un objeto del DOM.";
+            var userMessage = document.getElementById("userMessage");
+            userMessage.style.display = 'block';
+            return;
+        }
+        var currentProperty;
+        var propertiesTable = document.getElementById("propertiesTable")
+        var currentPropertyValue;
+        for (currentProperty in objeto) { //cada iteración genera una fila de la tabla
+            var currentPropertyTypeof = typeof objeto[currentProperty];
+            if ('object' === currentPropertyTypeof) {
+                currentPropertyValue = 'Es un objeto no imprimible'
+            } else if ('function' === currentPropertyTypeof) {
+                currentPropertyValue = 'Es una función no imprimible'
+            } else {
+                currentPropertyValue = objeto[currentProperty]
+            }
+            propertiesTable.innerHTML += "<tr><td>" + currentProperty + "</td><td> = " + currentPropertyValue + "</td></tr>";
+        }
+        var propertiesList = document.getElementById("propertiesList")
+        propertiesList.style.display = 'block';
     }
-}
-
-function num() {
-    return Math.ceil(Math.random() * 3);
-}
