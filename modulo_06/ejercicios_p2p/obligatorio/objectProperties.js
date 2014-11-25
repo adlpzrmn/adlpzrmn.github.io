83082358f6f667331b03acb5bc5db60cbb6a770c
adlpzrmn <adlpzrmn@yahoo.es>
2014-11-25 02:20:58 +0100

    [HTML5 MOOC] [Módulo 6] [Ejercicios P2P]
    Ejercicio obligatorio módulo 6

--- modulo_06/ejercicios_p2p/obligatorio/objectProperties.js
+++ modulo_06/ejercicios_p2p/obligatorio/objectProperties.js
@@ -3,32 +3,36 @@ function onchangeObjectName() {
     userMessage.style.display = 'none';
     var propertiesList = document.getElementById("propertiesList");
     propertiesList.style.display = 'none';
+    propertiesTable.innerHTML = "<tr><th> Propiedad </th><th> Valor </th></tr>";
 }
 
 function propertiesPrint(objectName) {
-        //alert('objectProperties.propertiesPrint() -> INICIO.');
-        var objeto = window[objectName];
-        if ('object' !== typeof objeto) {
-            var userMessageInline = document.getElementById("userMessageInline");
-            userMessageInline.innerHTML = "El elemento introducido no es un objeto del DOM.";
-            var userMessage = document.getElementById("userMessage");
-            userMessage.style.display = 'block';
-            return;
-        }
-        var currentProperty;
-        var propertiesTable = document.getElementById("propertiesTable")
-        var currentPropertyValue;
-        for (currentProperty in objeto) { //cada iteración genera una fila de la tabla
-            var currentPropertyTypeof = typeof objeto[currentProperty];
-            if ('object' === currentPropertyTypeof) {
-                currentPropertyValue = 'Es un objeto no imprimible'
-            } else if ('function' === currentPropertyTypeof) {
-                currentPropertyValue = 'Es una función no imprimible'
-            } else {
-                currentPropertyValue = objeto[currentProperty]
-            }
-            propertiesTable.innerHTML += "<tr><td>" + currentProperty + "</td><td> = " + currentPropertyValue + "</td></tr>";
-        }
-        var propertiesList = document.getElementById("propertiesList")
-        propertiesList.style.display = 'block';
+    //alert('objectProperties.propertiesPrint() -> INICIO.');
+    var objeto = window[objectName];
+    if ('object' !== typeof objeto) {
+        var userMessageInline = document.getElementById("userMessageInline");
+        userMessageInline.innerHTML = "El elemento introducido no es un objeto del DOM.";
+        var userMessage = document.getElementById("userMessage");
+        userMessage.style.display = 'block';
+        return;
     }
+    var currentProperty;
+    var propertiesTable = document.getElementById("propertiesTable");
+    var currentPropertyValue;
+    for (currentProperty in objeto) { //cada iteración genera una fila de la tabla
+        var currentPropertyTypeof = typeof objeto[currentProperty];
+        if ('object' === currentPropertyTypeof) {
+            currentPropertyValue = 'Es un objeto no imprimible';
+        } else if ('function' === currentPropertyTypeof) {
+            currentPropertyValue = 'Es una función no imprimible';
+        } else {
+            currentPropertyValue = objeto[currentProperty];
+        }
+        propertiesTable.innerHTML += "<tr><td>" + currentProperty + "</td><td> = " + currentPropertyValue + "</td></tr>";
+    }
+    var propertiesList = document.getElementById("propertiesList");
+    propertiesList.style.display = 'block';
+    var objectName = document.getElementById('objectName');
+    objectName.value = '';
+    return;
+}
